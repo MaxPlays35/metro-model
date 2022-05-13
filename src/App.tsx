54bd2665ba7883c5b07de7f5d2cc0f344f3062f6
MaxPlays35 <35427519+MaxPlays35@users.noreply.github.com>
2022-05-14 00:59:46 +0300

    Fix domain

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Stepper, Step, StepLabel } from '@mui/material'
 import trainImg from './subway.png'
 import { io } from "socket.io-client"
 
-const socket = io('http://127.0.0.1:8000/', {path: "/ws/socket.io"})
+const socket = io('http://trains.radolyn.com/', {path: "/api/ws/socket.io"})
 
 function App() {
 
